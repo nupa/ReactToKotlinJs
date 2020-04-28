93136a97eb06855539b9f8e2dd6a9bca894e8d42
Panu Akerman <panu.akerman@evident.fi>
2020-04-28 10:25:41 +0300

    Add simple css and dynamic lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
+import kotlinx.css.*
 import react.dom.*
+import styled.css
+import styled.styledDiv
 import kotlin.browser.document
 
 fun main() {
+    val unwatchedVideos = listOf(
+        Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+        Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+    )
+
+    val watchedVideos = listOf(
+        Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+    )
+
     render(document.getElementById("root")) {
         h1 {
             +"KotlinConf Explorer"
@@ -10,24 +23,27 @@ fun main() {
             h3 {
                 +"Videos to watch"
             }
-            p {
-                +"John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
+            for(video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
 
             h3 {
                 +"Videos watched"
             }
-            p {
-                +"Tom Jerry: Mouseless development"
+            for(video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
         }
-        div {
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
             h3 {
                 +"John Doe: Building and breaking things"
             }
