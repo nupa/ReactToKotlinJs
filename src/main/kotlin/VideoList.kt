2f2d44d5ca6ab23e2872479be56e6211e0312273
Panu Akerman <panu.akerman@evident.fi>
2020-04-28 18:01:39 +0300

    Add click action

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,16 +1,32 @@
 import kotlinx.css.p
+import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.p
+import kotlin.browser.window
 
 external interface VideoListProps: RProps {
     var videos: List<Video>
 }
 
-class VideoList: RComponent<VideoListProps, RState>() {
+external interface VideoListState: RState {
+    var selectedVideo: Video?
+}
+
+class VideoList: RComponent<VideoListProps, VideoListState>() {
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        setState {
+                            selectedVideo = video
+                        }
+                    }
+                }
+                if (video == state.selectedVideo) {
+                    +"▶ "
+                }
                 +"${video.speaker}: ${video.title}"
             }
         }
