fcaba143aa32d2092734cfa009089b733e426f3a
Panu Akerman <panu.akerman@evident.fi>
2020-04-29 21:39:21 +0300

    Fetch videos using coroutines

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,10 @@
+import kotlinx.coroutines.*
 import kotlinx.css.*
 import react.*
 import react.dom.*
 import styled.css
 import styled.styledDiv
+import kotlin.browser.window
 
 external interface AppState: RState {
     var currentVideo: Video?
@@ -13,14 +15,16 @@ external interface AppState: RState {
 class App : RComponent<RProps, AppState>() {
 
     override fun AppState.init() {
-        unwatchedVideos = listOf(
-            Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-            Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-            Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-        )
-        watchedVideos = listOf(
-            Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-        )
+        unwatchedVideos = listOf()
+        watchedVideos = listOf()
+
+        val mainScope = MainScope()
+        mainScope.launch {
+            val videos = fetchVideos()
+            setState {
+                unwatchedVideos = videos
+            }
+        }
     }
 
     override fun RBuilder.render() {
@@ -75,4 +79,20 @@ class App : RComponent<RProps, AppState>() {
             }
         }
     }
+}
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
+}
+
+suspend fun fetchVideo(id: Int): Video {
+    val responsePromise = window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+    val response = responsePromise.await()
+    val jsonPromise = response.json()
+    val json = jsonPromise.await()
+    return json.unsafeCast<Video>()
 }
